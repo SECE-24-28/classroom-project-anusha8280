491a7b351742de98c84b49108cc4060184765bca
Anusha S <smahk8280@gmail.com>
2025-12-03 15:24:54 +0530

    Add files via upload

---
+++ java Script progrm/demo3.js
@@ -0,0 +1,10 @@
+let a=10;
+let b="1";
+let c=5;
+
+a+=c;
+c**=2;
+console.log(a);
+console.log(c);
+console.log(a==b);
+console.log(a===b);
---
+++ java Script progrm/demo4.js
@@ -0,0 +1,6 @@
+let a=10;
+let b=45;
+let c=10;
+console.log(a<b && a===c);
+console.log(a>b || a===c);
+console.log(!(a<b && a===c));
